b3b334d19d452b4456b576f4f8d6ba60fc554c1e
Apena David <apenadavid232@gmail.com>
2026-06-05 19:16:30 +0100

    Update Dice Roller.js

--- Dice Roller.js
+++ Dice Roller.js
@@ -1,17 +1,26 @@
-function rollDice(){
-     
+function rollDice() {
+
     const numofdice = document.getElementById("numofdice").value;
-    const diceresult = document.getElementById("diceResult")
-    const diceImages = document.getElementById("diceImages")
-    const values = []
-    const images = []
+    const diceresult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
 
-    for(let i = 0; i < numofdice; i++){
-        const value = Math.floor(Math.random()*6) + 1
-        values.push(value)
-        images.push(`<img src"dice_images/dice${value}.webp.png>" alt = "Dice ${value}"`)
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numofdice; i++) {
+
+        const value = Math.floor(Math.random() * 6) + 1;
+
+        values.push(value);
+
+        images.push(`
+            <picture>
+                <source srcset="dice_images/${value}.webp" type="image/webp">
+                <img src="dice_images/${value}.png" alt="Dice ${value}">
+            </picture>
+        `);
     }
-}   
 
-diceresult.textcontent = `dice: ${values.join(`, `)}`;
-diceImages.innerHTML = images.join("")
+    diceresult.textContent = `Dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join("");
+}
